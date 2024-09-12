b1da397cbe3107f76af5097a6800ec14ef5ea087
Graham Duthie <grahamd483@gmail.com>
2024-09-12 13:01:02 +0200

    tracepoints added, workspace.xml committed to keep these.

--- xunit/bootstrap/bootstrap.py
+++ xunit/bootstrap/bootstrap.py
@@ -1,7 +1,12 @@
 class TestCase:
     def __init__(self, test_to_run):
         self.test_to_run: str = test_to_run
+
+    def setup(self):
+        pass
+
     def run(self):
+        self.setup()
         run_test_passed_to_this_test_case = getattr(self, self.test_to_run)
         run_test_passed_to_this_test_case()
 
@@ -9,15 +14,24 @@ class TestCase:
 class WasRun(TestCase):
     def __init__(self, test_to_run):
         self.was_run: bool = False
+        self.was_setup: bool = False
         super().__init__(test_to_run)
 
     def test_method(self):
         self.was_run = True
 
+    def setup(self):
+        self.was_setup = True
+
 
 class TestCaseTest(TestCase):
     def __init__(self, test_to_run):
         super().__init__(test_to_run)
+
+    def test_setup(self):
+        test = WasRun("test_method")
+        test.run()
+        assert(test.was_setup)
 
     def test_running(self):
         test = WasRun("test_method")
@@ -27,4 +41,5 @@ class TestCaseTest(TestCase):
         assert test.was_run
 
 
+TestCaseTest("test_setup").run()
 TestCaseTest("test_running").run()
