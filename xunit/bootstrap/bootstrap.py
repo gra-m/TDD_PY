80fd7a1c585f7b744e8c3fd0a7ffc3bfcd0a3b33
Graham Duthie <grahamd483@gmail.com>
2024-09-12 12:05:54 +0200

    attempting to make code self-explanatory with descriptive variable names

--- xunit/bootstrap/bootstrap.py
+++ xunit/bootstrap/bootstrap.py
@@ -1,12 +1,24 @@
-class WasRun:
-    def __init__(self, name):
+class TestCase:
+    def __init__(self, test_to_run):
+        self.test_to_run: str = test_to_run
+
+
+
+
+class WasRun(TestCase):
+    def __init__(self, test_to_run):
         self.was_run: bool = False
+        super().__init__(test_to_run)
 
     def test_method(self):
         self.was_run = True
 
+    def run(self):
+        run_test_passed_to_this_test_case = getattr(self, self.test_to_run)
+        run_test_passed_to_this_test_case()
+
 
 test = WasRun("test_method")
 print (test.was_run)
-test.test_method()
+test.run()
 print (test.was_run)
