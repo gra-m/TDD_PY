5f95a4c7f94903aeddfaecdbd7574c850586e794
Graham Duthie <grahamd483@gmail.com>
2024-09-12 15:59:09 +0200

    Tear down test working

--- xunit/bootstrap/bootstrap.py
+++ xunit/bootstrap/bootstrap.py
@@ -1,18 +1,23 @@
 from typing import Optional
 
-
 class TestCase:
     def __init__(self, test_to_run):
         self.test_to_run: str = test_to_run
 
     def setup(self):
-        # this is empty to allow self.setup to be called by TestCaseTest instances keeping run simple.
+        # this is empty to calls from all TestCaseTest instances keeping run simple.
+        pass
+
+    def tear_down(self):
+        # this is empty to calls from all TestCaseTest instances keeping run simple.
         pass
 
     def run(self):
+        # result.test_started() # added without explanation
         self.setup()
         run_test_passed_to_this_test_case = getattr(self, self.test_to_run)
         run_test_passed_to_this_test_case()
+        self.tear_down()
 
 
 class WasRun(TestCase):
@@ -20,15 +25,16 @@ class WasRun(TestCase):
         self.log = None
         super().__init__(test_to_run)
 
+    def setup(self):
+        # insert set up steps here
+        self.log = "setup_OK|"
+
     def test_method(self):
         # run tests here
         self.log = self.log + "test_method_OK|"
 
-    def setup(self):
-        # insert set up steps here
-        self.log = "setup_OK|"
-        
-
+    def tear_down(self):
+        self.log = self.log + "tear_down_OK|"
 
 class TestCaseTest(TestCase):
     def __init__(self, test_to_run):
@@ -38,7 +44,8 @@ class TestCaseTest(TestCase):
     def test_passed_to_this_test_case(self):
         self.test = WasRun("test_method")
         self.test.run()
-        assert "setup_OK|test_method_OK|"  == self.test.log
+        # runs silent == no output pass tracepoint can be run in debug
+        assert "setup_OK|test_method_OK|tear_down_OK|"  == self.test.log
 
 
 
